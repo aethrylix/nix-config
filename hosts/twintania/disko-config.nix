9317707c5cb3679ea2dd8d6ca20f12440ecc6543
Shane Wilson <github@dev.enahswlsn.com>
2026-01-12 17:47:16 +0000

    fix disko config

--- hosts/twintania/disko-config.nix
+++ hosts/twintania/disko-config.nix
@@ -15,7 +15,6 @@
                   type = "filesystem";
                   format = "vfat";
                   mountpoint = "/boot";
-                  mountOptions = [ "umask=0077" ];
                 };
               };
               root = {
